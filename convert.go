46d7c8a56df31fa1d50701e3fc552371482c0e36
Greg Lahaye <greg@lahayes.net>
2020-04-24 15:23:43 +0800

    Initial commit

---
+++ convert.go
@@ -0,0 +1,49 @@
+package convert
+
+import (
+	"golang.org/x/net/html"
+	"strconv"
+	"strings"
+)
+
+func IntToString(i int) string {
+	return strconv.Itoa(i)
+}
+
+func FloatToString(f float64) string {
+	return strconv.FormatFloat(f, 'f', 2, 64)
+}
+
+func PadString(s string, max int, left bool) string {
+	length := len(s)
+	if length > max {
+		return s
+	}
+
+	difference := max - length
+	padding := strings.Repeat(" ", difference)
+	if left {
+		s = padding + s
+	} else {
+		s += padding
+	}
+
+	return s
+}
+
+func ParseHTML(h string) string {
+	z := html.NewTokenizer(strings.NewReader(h))
+
+	var s strings.Builder
+	for {
+		tt := z.Next()
+		t := z.Token()
+
+		switch tt {
+		case html.ErrorToken:
+			return s.String()
+		case html.TextToken:
+			s.WriteString(t.Data)
+		}
+	}
+}
